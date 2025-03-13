d9ccb6f50e02e9e29ec6b3e92a4e7b995e473177
ColorOutOfSpace <20117570+rlpitts@users.noreply.github.com>
2025-03-13 20:40:14 +0100

    Create threaded_array_init.sh
    example to test in the morning

---
+++ exercises/matlab/threaded_array_init.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#Change to your local project number
+#SBATCH -A lu2025-7-34
+#SBATCH --output=matlab_threads_%J.out  # Output file
+#SBATCH --error=matlab_threads_%J.err   # Error file
+#Change time as needed
+#SBATCH --time=01:00:00                 # run time limit (HH:MM:SS)
+#SBATCH --nodes=1                         # node count
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4        #cpu-cores per task (>1 for multi-threaded)
+
+# Clean the environment
+module purge > /dev/null 2>&1
+
+# Change depending on resource and MATLAB version
+# to find out available versions: module spider matlab
+module load matlab/R2023b
+
+## Run your program or script
+matlab -nodisplay  -nosplash -nodesktop -r "threaded_arrinit"
