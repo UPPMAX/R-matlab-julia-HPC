627dbb77c358adb8e367c990e9f7b0e2db7fa371
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-09-04 18:16:32 +0200

    Improve scripts

--- docs-mk/advanced/parallel/run_pelle_1.sh
+++ docs-mk/advanced/parallel/run_pelle_1.sh
@@ -6,16 +6,5 @@
 #SBATCH --time=00:10:00 # maximum execution time of
 #SBATCH --ntasks=1
 #SBATCH --output=run_pelle_1.out
-
-filename=topol.tpr
-url=https://github.com/UPPMAX/R-matlab-julia-HPC/raw/refs/heads/main/docs-mk/lesson_plans/20251010_richel/lumi_course/topol.tpr
-
-if [ -e "${filename}" ]; then
-  echo "File '${filename}' already exists. Skipping download."
-else
-  echo "Downloading '${filename}' from '${url}'..."
-  wget -O "${filename}" "${url}"
-fi
-
 module load GROMACS/2024.4-foss-2023b
 srun gmx_mpi mdrun -g run_pelle_1 -nsteps -1 -maxh 0.017 -resethway -notunepme
