535541264be0e1cc7e8c616b2fb21856f6ba8d42
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-09-04 18:23:25 +0200

    Try mpirun

--- docs-mk/advanced/parallel/run_pelle_1.sh
+++ docs-mk/advanced/parallel/run_pelle_1.sh
@@ -7,4 +7,5 @@
 #SBATCH --ntasks=1
 #SBATCH --output=run_pelle_1.out
 module load OpenMPI/5.0.7-GCC-14.2.0 GROMACS/2024.4-foss-2023b
-srun gmx_mpi mdrun -g run_pelle_1 -nsteps -1 -maxh 0.017 -resethway -notunepme
+# srun gmx_mpi mdrun -g run_pelle_1 -nsteps -1 -maxh 0.017 -resethway -notunepme
+mpirun gmx_mpi mdrun -g run_pelle_1 -nsteps -1 -maxh 0.017 -resethway -notunepme
