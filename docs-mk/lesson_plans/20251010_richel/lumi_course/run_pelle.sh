cbeb91a18a55111fc5b0f4248f6c75c083ae2735
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-09-04 15:05:49 +0200

    Improve

--- docs-mk/lesson_plans/20251010_richel/lumi_course/run_pelle.sh
+++ docs-mk/lesson_plans/20251010_richel/lumi_course/run_pelle.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#
+# Run GROMACS on an example file
+#
 
 filename=topol.tpr
 url=https://github.com/UPPMAX/R-matlab-julia-HPC/raw/refs/heads/main/docs-mk/lesson_plans/20251010_richel/lumi_course/topol.tpr
@@ -9,3 +12,5 @@ else
   echo "Downloading '${filename}' from '${url}'..."
   wget -O "${filename}" "${url}"
 fi
+
+module load GROMACS/2024.4-foss-2023b
