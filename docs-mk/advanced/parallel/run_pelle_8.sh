15e45aa97647ab35974647868c74d3394dd1d732
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-09-04 18:28:10 +0200

    Specify n flag

--- docs-mk/advanced/parallel/run_pelle_8.sh
+++ docs-mk/advanced/parallel/run_pelle_8.sh
@@ -7,4 +7,4 @@
 #SBATCH --ntasks=8
 #SBATCH --output=run_pelle_8.out
 module load OpenMPI/5.0.7-GCC-14.2.0 GROMACS/2024.4-foss-2023b
-mpirun gmx_mpi mdrun -g run_pelle_8 -nsteps -1 -maxh 0.017 -resethway -notunepme
+mpirun -n 8 gmx_mpimdrun -g run_pelle_8 -nsteps -1 -maxh 0.017 -resethway -notunepme
