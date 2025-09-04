86ed62e6348f488e46a2c5dc9ab34e32c40278a0
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-09-04 15:13:56 +0200

    Single core job

--- docs-mk/lesson_plans/20251010_richel/lumi_course/run_pelle.sh
+++ docs-mk/lesson_plans/20251010_richel/lumi_course/run_pelle.sh
@@ -3,6 +3,10 @@
 # Run GROMACS on an example file
 #
 #SBATCH -A staff
+#SBATCH --time=00:10:00 # maximum execution time of
+#SBATCH --nodes=1 # we run on 1 node
+#SBATCH --ntasks-per-node=1 # 1 MPI rank
+#SBATCH --cpus-per-task=1 # number cpus-per-task
 
 filename=topol.tpr
 url=https://github.com/UPPMAX/R-matlab-julia-HPC/raw/refs/heads/main/docs-mk/lesson_plans/20251010_richel/lumi_course/topol.tpr
@@ -15,3 +19,7 @@ else
 fi
 
 module load GROMACS/2024.4-foss-2023b
+
+gmx_mpi mdrun \
+  -g ex1.1_${SLURM_NTASKS}x${OMP_NUM_THREADS}_jID${SLURM_JOB_ID} \
+  -nsteps -1 -maxh 0.017 -resethway -notunepme
