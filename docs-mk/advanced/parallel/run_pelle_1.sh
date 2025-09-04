25e7473084658570a701df11bde0b82090da6699
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-09-04 18:21:25 +0200

    Use MPI

--- docs-mk/advanced/parallel/run_pelle_1.sh
+++ docs-mk/advanced/parallel/run_pelle_1.sh
@@ -6,5 +6,5 @@
 #SBATCH --time=00:10:00 # maximum execution time of
 #SBATCH --ntasks=1
 #SBATCH --output=run_pelle_1.out
-module load GROMACS/2024.4-foss-2023b
+module load OpenMPI/5.0.7-GCC-14.2.0 GROMACS/2024.4-foss-2023b
 srun gmx_mpi mdrun -g run_pelle_1 -nsteps -1 -maxh 0.017 -resethway -notunepme
--- docs-mk/advanced/parallel/run_pelle_8.sh
+++ docs-mk/advanced/parallel/run_pelle_8.sh
@@ -6,3 +6,5 @@
 #SBATCH --time=00:10:00 # maximum execution time of
 #SBATCH --ntasks=8
 #SBATCH --output=run_pelle_8.out
+module load OpenMPI/5.0.7-GCC-14.2.0 GROMACS/2024.4-foss-2023b
+srun gmx_mpi mdrun -g run_pelle_8 -nsteps -1 -maxh 0.017 -resethway -notunepme
