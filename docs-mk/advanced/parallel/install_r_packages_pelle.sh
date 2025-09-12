798f6da8f877a706767db2dafbaec40dd81628ae
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-09-12 06:59:40 +0200

    Specify mirror

--- docs-mk/advanced/parallel/install_r_packages_pelle.sh
+++ docs-mk/advanced/parallel/install_r_packages_pelle.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #SBATCH -A staff
 module load R
-Rscript -e 'install.packages(c("testthat", "doParallel"))'
+Rscript -e 'install.packages(c("testthat", "doParallel"), repos = "http://cran.us.r-project.org"))'
