b66d785fc95a4c5c617ded83eb92b852eb64a951
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-09-12 07:00:19 +0200

    Fix script

--- docs-mk/advanced/parallel/install_r_packages_pelle.sh
+++ docs-mk/advanced/parallel/install_r_packages_pelle.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #SBATCH -A staff
 module load R
-Rscript -e 'install.packages(c("testthat", "doParallel"), repos = "http://cran.us.r-project.org"))'
+Rscript -e 'install.packages(c("testthat", "doParallel"), repos = "http://cran.us.r-project.org")'
