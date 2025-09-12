96ca1aafdd5ce29cfa47d5572dd86eb699989cc4
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-09-12 07:58:43 +0200

    Lower to max

--- docs-mk/advanced/parallel/pelle_run_r_script_1.sh
+++ docs-mk/advanced/parallel/pelle_run_r_script_1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -n 1
 #SBATCH -A staff
-#SBATCH --time=230:00:00
+#SBATCH --time=10:00:00
 #SBATCH --output "pelle_1.txt"
 module load R
 Rscript r_script.R ${SLURM_NTASKS}
--- docs-mk/advanced/parallel/pelle_run_r_script_16.sh
+++ docs-mk/advanced/parallel/pelle_run_r_script_16.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -n 16
 #SBATCH -A staff
-#SBATCH --time=230:00:00
+#SBATCH --time=10:00:00
 #SBATCH --output "pelle_16.txt"
 module load R
 Rscript r_script.R ${SLURM_NTASKS}
