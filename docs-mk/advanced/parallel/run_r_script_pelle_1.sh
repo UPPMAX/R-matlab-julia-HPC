12f739da03d4fbdfad7c90388268e7355bde0f00
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-09-12 07:03:53 +0200

    Use fixed job names

--- docs-mk/advanced/parallel/run_r_script_pelle_1.sh
+++ docs-mk/advanced/parallel/run_r_script_pelle_1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #SBATCH -n 1
 #SBATCH -A staff
+#SBATCH --output "pelle_1.txt"
 module load R R_packages
 Rscript r_script.R ${SLURM_NTASKS}
--- docs-mk/advanced/parallel/run_r_script_pelle_16.sh
+++ docs-mk/advanced/parallel/run_r_script_pelle_16.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #SBATCH -n 16
 #SBATCH -A staff
+#SBATCH --output "pelle_16.txt"
 module load R R_packages
 Rscript r_script.R ${SLURM_NTASKS}
