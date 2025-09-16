e31ea631887ea2e62ef50a4aa7e8d370ccd3fe32
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-09-16 13:21:58 +0200

    Run script directly

--- docs-mk/lesson_plans/20251010_richel/6_integration2d/schedule_rackham_runs.sh
+++ docs-mk/lesson_plans/20251010_richel/6_integration2d/schedule_rackham_runs.sh
@@ -2,14 +2,33 @@
 #
 # Usage:
 #
-#   sbatch -A [account] schedule_rackham_runs.sh
+#   ./schedule_rackham_runs.sh [account]
 #
 # Example:
 #
-#   sbatch -A staff schedule_rackham_runs.sh
+#   ./schedule_rackham_runs.sh staff
 #
-echo "Slurm job account used: ${SLURM_JOB_ACCOUNT}"
-sbatch -A ${SLURM_JOB_ACCOUNT} -n 1 integration2d_rackham.sh
-sbatch -A ${SLURM_JOB_ACCOUNT} -n 4 integration2d_rackham.sh
-sbatch -A ${SLURM_JOB_ACCOUNT} -n 16 integration2d_rackham.sh
-sbatch -A ${SLURM_JOB_ACCOUNT} -n 63 integration2d_rackham.sh
+
+if [ "$#" -ne 1 ]; then
+    echo "ERROR: Incorrect number of arguments"
+    echo "when calling 'schedule_rackham_runs.sh'."
+    echo " "
+    echo "Please add an argument with the name of the Slurm job account::"
+    echo " "
+    echo "Usage:"
+    echo " "
+    echo "  ./schedule_rackham_runs.sh [account]"
+    echo " "
+    echo "Examples:"
+    echo " "
+    echo "  ./schedule_rackham_runs.sh staff"
+    echo " "
+fi
+
+slurm_job_account=$1
+echo "Slurm job account used: ${slurm_job_account}"
+
+sbatch -A ${slurm_job_account} -n 1 integration2d_rackham.sh
+sbatch -A ${slurm_job_account} -n 4 integration2d_rackham.sh
+sbatch -A ${slurm_job_account} -n 16 integration2d_rackham.sh
+sbatch -A ${slurm_job_account} -n 64 integration2d_rackham.sh
