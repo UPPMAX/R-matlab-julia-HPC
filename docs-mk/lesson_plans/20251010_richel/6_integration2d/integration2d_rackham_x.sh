5e46546b96dc50780b99f153747a1f6eb63cb736
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-09-16 09:32:39 +0200

    Hardcode staff

--- docs-mk/lesson_plans/20251010_richel/6_integration2d/integration2d_rackham_x.sh
+++ docs-mk/lesson_plans/20251010_richel/6_integration2d/integration2d_rackham_x.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#SBATCH --time=00:20:00      # requested time
+#SBATCH -A staff
+#SBATCH --time=00:20:00
 module load R_packages/4.1.1 >/dev/null 2>&1
 
 echo "Number of cores booked in Slurm: ${SLURM_NPROCS}"
