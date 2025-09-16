aebc1bc422374ffb67e94da33276dd7eabab2e43
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-09-16 09:25:28 +0200

    Experiment

---
+++ docs-mk/lesson_plans/20251010_richel/6_integration2d/integration2d_rackham_x.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH -n 1           # nr. tasks/coresw
+#SBATCH -A staff
+#SBATCH --time=00:20:00      # requested time
+module load R_packages/4.1.1 >/dev/null 2>&1
+
+echo "Number of cores booked in Slurm: ${SLURM_NPROCS}"
+Rscript --no-save --no-restore integration2d.R 1
---
+++ docs-mk/lesson_plans/20251010_richel/6_integration2d/schedule_rackham_runs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+./integration2d_rackham_x.sh -A staff -n 1
+./integration2d_rackham_x.sh -A staff -n 2
+./integration2d_rackham_x.sh -A staff -n 4
+./integration2d_rackham_x.sh -A staff -n 8
+./integration2d_rackham_x.sh -A staff -n 16
+./integration2d_rackham_x.sh -A staff -n 32
