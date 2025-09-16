c5edfebc1e39be13f193dd0d04a8c07e25506e9f
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-09-16 10:59:04 +0200

    Account first

--- docs-mk/lesson_plans/20251010_richel/6_integration2d/integration2d-cosmos.sh
+++ docs-mk/lesson_plans/20251010_richel/6_integration2d/integration2d-cosmos.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
+#SBATCH -A lu2025-7-76
 #SBATCH -n 1                 # nr. tasks
-#SBATCH -A lu2025-7-76      # your project_ID
 #SBATCH --time=00:20:00      # requested time
-
 ml purge > /dev/null 2>&1
 ml GCC/11.3.0  OpenMPI/4.1.4  R/4.2.1
 Rscript --no-save --no-restore do_2d_integration.R
