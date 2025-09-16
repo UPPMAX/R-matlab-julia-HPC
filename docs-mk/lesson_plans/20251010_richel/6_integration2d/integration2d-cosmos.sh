2abac159ea74801c511db9d2e22e40c148a10aa0
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-09-16 10:56:03 +0200

    Use correct project

--- docs-mk/lesson_plans/20251010_richel/6_integration2d/integration2d-cosmos.sh
+++ docs-mk/lesson_plans/20251010_richel/6_integration2d/integration2d-cosmos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -n 1                 # nr. tasks
-#SBATCH -A lu2025-7-24      # your project_ID
+#SBATCH -A lu2025-7-76      # your project_ID
 #SBATCH --time=00:20:00      # requested time
 
 ml purge > /dev/null 2>&1
