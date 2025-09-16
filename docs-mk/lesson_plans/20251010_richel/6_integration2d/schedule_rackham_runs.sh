48f70fd9475e5c846288255874635fe7604d36cb
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-09-16 13:11:32 +0200

    sbatch from CLI

--- docs-mk/lesson_plans/20251010_richel/6_integration2d/schedule_rackham_runs.sh
+++ docs-mk/lesson_plans/20251010_richel/6_integration2d/schedule_rackham_runs.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+#
+# Usage:
+#
+#   sbatch -A [account] schedule_rackham_runs.sh
+#
+# Example:
+#
+#   sbatch -A staff schedule_rackham_runs.sh
+#
+
+echo "Slurm job accoun used: ${SLURM_JOB_ACCOUNT}"
+sbatch -A ${SLURM_JOB_ACCOUNT} -n 2 integration2d_rackham_x.sh
+
+exit
+
 for filename in $(ls integration2d_rackham_*.sh)
 do
   sbatch ${filename}
