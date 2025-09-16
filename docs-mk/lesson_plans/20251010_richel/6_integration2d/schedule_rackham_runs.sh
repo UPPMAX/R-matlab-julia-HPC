00d5b8b469a03a1d7917aed0cdd23cb8f2c3810e
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-09-16 10:00:10 +0200

    Use correct Slurm flags

--- docs-mk/lesson_plans/20251010_richel/6_integration2d/schedule_rackham_runs.sh
+++ docs-mk/lesson_plans/20251010_richel/6_integration2d/schedule_rackham_runs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sbatch integration2d_rackham_x.sh -A staff -n 1
-sbatch integration2d_rackham_x.sh -A staff -n 2
-sbatch integration2d_rackham_x.sh -A staff -n 4
-sbatch integration2d_rackham_x.sh -A staff -n 8
-sbatch integration2d_rackham_x.sh -A staff -n 16
-sbatch integration2d_rackham_x.sh -A staff -n 32
+sbatch integration2d_rackham_x.sh -A staff --cpus-per-task 1
+sbatch integration2d_rackham_x.sh -A staff --cpus-per-task 2
+sbatch integration2d_rackham_x.sh -A staff --cpus-per-task 4
+sbatch integration2d_rackham_x.sh -A staff --cpus-per-task 8
+sbatch integration2d_rackham_x.sh -A staff --cpus-per-task 16
+sbatch integration2d_rackham_x.sh -A staff --cpus-per-task 32
