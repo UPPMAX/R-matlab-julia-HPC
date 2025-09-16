0d690e6f22ad3011cdbd237d9c5415ff3e43e5af
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-09-16 13:03:40 +0200

    Use CLI sbatch

---
+++ docs-mk/lesson_plans/20251010_richel/6_integration2d/integration2d_rackham_x.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --time=02:00:00
+#
+# Usage:
+#
+#   sbatch -A staff -n 1 integration2d_rackham_x.sh
+#
+
+module load R_packages/4.1.1 >/dev/null 2>&1
+
+echo "Number of cores booked in Slurm: ${SLURM_NPROCS}"
+Rscript --no-save --no-restore do_2d_integration.R ${SLURM_NPROCS}
