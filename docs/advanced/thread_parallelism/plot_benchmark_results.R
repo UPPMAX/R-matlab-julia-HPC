4d7e143a0a95684d9c2501ff38c8c4f35fba29c9
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-09-18 14:16:05 +0200

    Plot Pelle results

--- docs/advanced/thread_parallelism/plot_benchmark_results.R
+++ docs/advanced/thread_parallelism/plot_benchmark_results.R
@@ -12,11 +12,11 @@ t <- dplyr::bind_rows(list_of_tables)
 
 ggplot2::ggplot(
   t,
-  ggplot2::aes(x = n_workers, y = core_secs, color = language)
+  ggplot2::aes(x = n_workers, y = core_secs, color = hpc_cluster)
 ) + ggplot2::geom_point() +
   ggplot2::scale_x_continuous("Number of workers", limits = c(0, max(t$n_workers))) +
   ggplot2::scale_y_continuous("Core seconds", limits = c(0, max(t$core_secs))) +
-  ggplot2::facet_grid(hpc_cluster ~ .) +
+  ggplot2::facet_grid(language ~ .) +
   ggplot2::labs(title = "Threaded parallelism")
 
 ggplot2::ggsave("benchmark_results.png", width = 7, height = 7)
