c8d138b0a1022cdffb4d59a92b82698517456c29
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-09-19 11:31:54 +0200

    Plot more results

--- docs/advanced/thread_parallelism/plot_benchmark_results.R
+++ docs/advanced/thread_parallelism/plot_benchmark_results.R
@@ -23,9 +23,9 @@ ggplot2::ggplot(
   ggplot2::aes(x = n_workers, y = core_secs, color = hpc_cluster)
 ) + ggplot2::geom_point() +
   ggplot2::scale_x_continuous("Number of workers", limits = c(0, max(t$n_workers))) +
-  ggplot2::scale_y_continuous("Core seconds", limits = c(0, max(t$core_secs))) +
+  ggplot2::scale_y_continuous("Core seconds", limits = c(0, NA)) +
   ggplot2::geom_smooth() +
-  ggplot2::facet_grid(language ~ .) +
+  ggplot2::facet_grid(language ~ ., scales = "free_y") +
   ggplot2::labs(
     title = "Threaded parallelism",
     caption = paste0("Number of runs: ", nrow(t))
