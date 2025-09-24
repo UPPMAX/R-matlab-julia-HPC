10d42880c3b184a0288395248ea62711243742e7
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-09-24 14:55:38 +0200

    It is not possible

--- docs/advanced/thread_parallelism/do_2d_integration.R
+++ docs/advanced/thread_parallelism/do_2d_integration.R
@@ -1,20 +1,21 @@
 #!/bin/env Rscript
 
 # Step 1: install all packages
+if ("this" == "actually possible") {
+  #' Make the script more English
+  is_package_installed <- function(package_name) {
+    require(package_name)
+  }
 
-#' Make the script more English
-is_package_installed <- function(package_name) {
-  require(package_name)
-}
-
-if (!is_package_installed("testthat")) {
-  install.packages("testthat", repos = "http://cran.us.r-project.org")
-}
-if (!is_package_installed("doParallel")) {
-  install.packages("doParallel", repos = "http://cran.us.r-project.org")
-}
-if (!is_package_installed("stringr")) {
-  install.packages("stringr", repos = "http://cran.us.r-project.org")
+  if (!is_package_installed("testthat")) {
+    install.packages("testthat", repos = "http://cran.us.r-project.org")
+  }
+  if (!is_package_installed("doParallel")) {
+    install.packages("doParallel", repos = "http://cran.us.r-project.org")
+  }
+  if (!is_package_installed("stringr")) {
+    install.packages("stringr", repos = "http://cran.us.r-project.org")
+  }
 }
 
 
